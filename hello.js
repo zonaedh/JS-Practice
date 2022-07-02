5ada9687d1b464b0380d4a04104cf926344e2541
zonaedhossain <zonaed.ain@gmail.com>
2022-07-03 00:42:24 +0600

    first commit

---
+++ hello.js
@@ -0,0 +1,21 @@
+//Biyar code
+var jobPaiso = false;
+var savingsAmount = 10000;
+if(jobPaiso == true || savingsAmount > 10000){
+    console.log("Aso tmr patri khuji");
+}
+else if (jobPaiso == true){
+ console.log("acccha koydin por dekhbo");
+}
+else if(savingsAmount == 10000){
+    console.log("taka owa maal tmi, biya confirm!!");
+}
+else{
+    console.log("tor kopale biya naai mona");
+}
+var date = new Date;
+console.log(date)
+
+var friendsAge = [20,27,22,29,50,75];
+var zonaed = [2];
+console.log(zonaed);
