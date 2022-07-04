81d066533e1ac55470152cf082a5b4ec4f2f1ff8
zonaedhossain <zonaed.ain@gmail.com>
2022-07-05 00:25:46 +0600

    update

--- function.js
+++ function.js
@@ -1,13 +1,20 @@
-function heyZonaed(){
-    console.log("Hey zonaed")
+// function heyZonaed(){
+//     console.log("Hey zonaed")
+// }
+// function thanks(){
+//     console.log("Ok, thanks");
+// }
+// heyZonaed();
+// console.log("What's your task deadline?");
+// const date = 29;
+// console.log("It's 29 july");
+// const task = "Done my task";
+// const salary = "paid";
+// thanks();
+function doubleIt(num){
+    const result = num * 2;
+    console.log(result);
 }
-function thanks(){
-    console.log("Ok, thanks");
-}
-heyZonaed();
-console.log("What's your task deadline?");
-const date = 29;
-console.log("It's 29 july");
-const task = "Done my task";
-const salary = "paid";
-thanks();
+doubleIt(5);
+doubleIt(50);
+doubleIt(1000);
