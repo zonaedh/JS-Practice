e29bf8f4f3781250e00c025c92d5fb462bb314cc
zonaedhossain <zonaed.ain@gmail.com>
2022-07-05 02:56:51 +0600

    object

---
+++ object.js
@@ -0,0 +1,7 @@
+const student = {id:121, phone:6659, name:"zonaed"}
+const student2 = {id:232, phone:5859, name:"hossain"}
+
+// Update Phone Number
+student.phone = 01979996659;
+const phoneNumber = student.phone;
+    console.log(student);
