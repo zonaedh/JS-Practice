07052b8c2562ce0de5449336dbdef376174db37c
zonaedhossain <zonaed.ain@gmail.com>
2022-07-05 03:14:19 +0600

    inchToFeet

---
+++ inchToFeet.js
@@ -0,0 +1,14 @@
+// const inch = 290;
+//     result = 290 /12;
+
+//     console.log(length)
+    
+// Using Function
+function inchToFeet(inch){
+    const feet = inch /12;
+    return feet;
+}
+const nanaVai = inchToFeet(588);
+console.log(nanaVai)
+const daduVai = inchToFeet(288);
+console.log(daduVai)
