e3b16d9d5d09f8633444cab0860228c06f54857c
zonaedhossain <zonaed.ain@gmail.com>
2022-07-05 02:36:03 +0600

    update

--- function.js
+++ function.js
@@ -1,20 +1,36 @@
-// function heyZonaed(){
-//     console.log("Hey zonaed")
-// }
-// function thanks(){
-//     console.log("Ok, thanks");
-// }
-// heyZonaed();
-// console.log("What's your task deadline?");
-// const date = 29;
-// console.log("It's 29 july");
-// const task = "Done my task";
-// const salary = "paid";
-// thanks();
+function heyZonaed(){
+    console.log("Hey zonaed")
+}
+function thanks(){
+    console.log("Ok, thanks");
+}
+heyZonaed();
+console.log("What's your task deadline?");
+const date = 29;
+console.log("It's 29 july");
+const task = "Done my task";
+const salary = "paid";
+thanks();
+
+// function new:
 function doubleIt(num){
     const result = num * 2;
-    console.log(result);
+    return result;
+ // console.log(result);
 }
 doubleIt(5);
 doubleIt(50);
 doubleIt(1000);
+
+const first = doubleIt(5);
+const second = doubleIt(20);
+const total = first + second;
+console.log(total);
+
+// multi add
+function add(sum1, sum2){
+    const result = sum1 + sum2;
+    return result;
+}
+const sum = add(10, 20);;
+    console.log(sum)
